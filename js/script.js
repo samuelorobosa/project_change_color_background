b66e1f758c5dd203abd2067bcf823eb52acf6f40
Samuel Amagbakhen <amagbakhensamuel@gmail.com>
2020-05-19 14:43:39 +0100

    CSS Gradient
    How about a CSS gradient color picker?

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
-//Choose a random color
+//Choose random colors
 const button = document.querySelector('button')
 const body = document.querySelector('body')
-const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple']
+const colors = ['red', 'green', 'blue', 'yellow', 'pink', 'purple'];
+const colors2=['black', 'blue', 'grey','white', 'yellow', 'fuchsia']; //second color array
 
-body.style.backgroundColor = 'violet'
+body.style.backgroundImage = 'linear-gradient(to right, red, yellow)'; //Css gradient
 button.addEventListener('click', changeBackground)
 
 function changeBackground(){
 const colorIndex= parseInt(Math.random()*colors.length)
-body.style.backgroundColor = colors[colorIndex]
+const colorIndex2= parseInt(Math.random()*colors2.length) //Second color picker
+body.style.backgroundImage = 'linear-gradient(to right,'+ colors[colorIndex] + ',' + colors2[colorIndex2] + ')';
 }
